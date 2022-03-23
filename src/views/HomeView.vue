27e48c044c7a9055bfaf2d580ce747f5f79b667a
ablancaa <88708431+ablancaa@users.noreply.github.com>
2022-03-23 16:10:32 +0100

    Añado imagen

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
+    <img alt="Vue logo" src="../assets/FotoPerfil.jpg" height="150">
    <testElement :size="count" />
      </div>
 </template>
