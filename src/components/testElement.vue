3f954339059538f6ba874c56e0a87e5ed413df3b
ablancaa <88708431+ablancaa@users.noreply.github.com>
2022-03-23 15:56:23 +0100

    pruebas

---
+++ src/components/testElement.vue
@@ -0,0 +1,45 @@
+<template>
+    <div>
+        <h1 v-if="esPar">I am odd</h1>
+        <h1 v-else>{{count}}</h1>
+        <br/><br/>
+          <button class="btn" @click="increaseCount(size)">click me!</button>
+    </div>
+</template>
+
+<script>    
+    export default {
+        name: "testElement",
+        elements: Number,
+        color: String,
+        components: {},
+        props: {},
+        data(){
+            return{
+                esPar: true,
+                count: 6,
+                size: 1,
+                values: [
+                    { id: 1, name: "AAA" },
+                    { id: 2, name: "BBB" },
+                ],
+            };
+        }, methods: {
+            increaseCount: function () {
+                this.count=this.count+this.size;
+                if((this.count % 3) == 0){
+                    this.count+1;
+                    this.esPar = false;
+                    console.log(this.count+" Es múltiplo de 3")
+                } else {
+                    this.esPar= true;
+                    console.log(this.count+" NO es múltiplo de 3")
+                }
+            },
+        },
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,38 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+   <testElement :size="count" />
+     </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+//import HelloWorld from '@/components/HelloWorld.vue'
+import testElement from "@/components/testElement.vue";
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
-  }
+    testElement,
+  }, 
+  data(){}
 }
 </script>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
